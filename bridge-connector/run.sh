7cad6986c3e5c67a4e01ce0071ea5c14f3f9b39e
majchrow <dmajchrowski6@gmail.com>
2020-04-24 19:01:48 +0200

    Add server streams

--- bridge-connector/run.sh
+++ bridge-connector/run.sh
@@ -6,6 +6,7 @@ source .venv/bin/activate
 pip install -r requirements.txt
 echo 'Running server'
 python3 main.py &
+sleep 3
 echo 'Running client'
 python3 main.py --client
 sleep 3
--- bridge-connector/server.py
+++ bridge-connector/server.py
@@ -1,5 +1,7 @@
 import asyncio
+import json
 import logging
+from time import sleep
 
 from websockets import WebSocketServerProtocol, serve
 
@@ -10,21 +12,42 @@ class Server:
     def __init__(self, FLAGS) -> None:
         self.FLAGS = FLAGS
         self.clients = set()
+        self.call_info_clients = []
+        self.call_roaster_clients = []
+        self.calls_clients = []
+        self.call_info_data = []
+        self.call_roaster_data = []
+        self.calls_data = []
+        self.unknown = []
+        self.loop = None
         enable_logger(directory="logs/server", filename="server")
+        self._prepare_data()
 
     def start(self) -> None:
         start_server = serve(self.ws_handler, self.FLAGS.host, self.FLAGS.port)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(start_server)
+        self.loop = asyncio.get_event_loop()
+        self.loop.run_until_complete(start_server)
         logging.info(f"Websocket server listening on port {self.FLAGS.host}:{self.FLAGS.port}.")
-        loop.run_forever()
+        sleep(3)
+        self.start_streams()
+        self.loop.run_forever()
+
+    def _prepare_data(self) -> None:
+        with open(self.FLAGS.logfile) as json_file:
+            data = json.load(json_file)
+        for message in data:
+            queue = self.unknown
+            if message['message']['type'] == "rosterUpdate":
+                queue = self.call_roaster_data
+            elif message['message']['type'] == "callInfoUpdate":
+                queue = self.call_info_data
+            elif message['message']['type'] == "callListUpdate":
+                queue = self.calls_data
+            queue.append(message)
 
     async def ws_handler(self, ws: WebSocketServerProtocol, url: str) -> None:
         await self.register(ws)
-        try:
-            await self.request_handler(ws)
-        finally:
-            await self.unregister(ws)
+        await self.handler(ws)
 
     async def register(self, ws: WebSocketServerProtocol) -> None:
         self.clients.add(ws)
@@ -34,7 +57,33 @@ class Server:
         self.clients.remove(ws)
         logging.info(f"{ws.remote_address} disconnected.")
 
-    async def request_handler(self, ws: WebSocketServerProtocol) -> None:
-        msg = await ws.recv()
-        print(f"Received {msg}")
-        await ws.send("hello from server")
+    def start_streams(self) -> None:
+        self.loop.create_task(self.stream_call_info())
+        self.loop.create_task(self.stream_call_roaster())
+        self.loop.create_task(self.stream_calls())
+
+    async def handler(self, ws: WebSocketServerProtocol) -> None:
+        while True:
+            msg = await ws.recv()
+            logging.info(f"Got from {ws} message {msg}")
+
+    async def stream_call_info(self) -> None:
+        for data in self.call_info_data:
+            logging.info(data)
+            for client in self.call_info_clients:
+                await client.send(data)
+            await asyncio.sleep(2)
+
+    async def stream_call_roaster(self) -> None:
+        for data in self.call_roaster_data:
+            logging.info(data)
+            for client in self.call_roaster_clients:
+                await client.send(data)
+            await asyncio.sleep(2)
+
+    async def stream_calls(self) -> None:
+        for data in self.calls_data:
+            logging.info(data)
+            for client in self.calls_clients:
+                await client.send(data)
+            await asyncio.sleep(2)
