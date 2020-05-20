e5fa163ec467d78e32e669ce95a30453a5de9f33
majchrow <dmajchrowski6@gmail.com>
2020-05-20 14:39:37 +0200

    Add authentication token

--- bridge-connector/main.py
+++ bridge-connector/main.py
@@ -10,27 +10,31 @@ def create_parser():
                         type=str,
                         default='localhost',
                         help='host ip')
+    parser.add_argument('--auth_token',
+                        type=str,
+                        help='authentication token')
     parser.add_argument('--logfile',
                         type=str,
                         default='log.json',
-                        help='host ip')
+                        help='logfile')
     parser.add_argument('--port',
                         type=int,
                         default=12345,
                         help='port number')
-    parser.add_argument('--client',
-                        dest='client',
+    parser.add_argument('--server',
+                        dest='server',
                         action='store_true',
-                        help='run client, if not set run server')
+                        help='run server, by default runs client')
     return parser
 
 
 if __name__ == '__main__':
     parser = create_parser()
     FLAGS = parser.parse_args()
-    if FLAGS.client:
-        client = Client(FLAGS)
-        client.start()
-    else:
+    assert FLAGS.server or FLAGS.auth_token, "Authentication token is required for client"
+    if FLAGS.server:
         server = Server(FLAGS)
         server.start()
+    else:
+        client = Client(FLAGS)
+        client.start()
