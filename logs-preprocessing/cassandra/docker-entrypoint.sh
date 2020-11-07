9c7f21d9900d6aa095b51bae0f6d2117636f830f
Jolanta Gluza <>
2020-11-07 22:47:17 +0100

    Dockerize logs-preprocessing app

--- logs-preprocessing/cassandra/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ ! -z "$CASSANDRA_KEYSPACE" && $1 = 'cassandra' ]]; then
-    CQL="CREATE KEYSPACE $CASSANDRA_KEYSPACE WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}; USE $CASSANDRA_KEYSPACE; CREATE TABLE $TABLE(id text PRIMARY KEY, date text, call_id text, initial text, ringing text, connected text, onhold text, activespeaker text, presenter text, endpointrecording text);"
-    until echo $CQL | cqlsh; do
-        echo "cqlsh: Cassandra is unavailable - retry later"
-        sleep 2
-    done &
-fi
