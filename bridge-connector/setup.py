c19a63a152b1f610252b2d192fd0264776071ada
enthyp <kuba.lanecki@gmail.com>
2020-11-07 16:58:21 +0100

    More logging, some exception handling, simplified token management.

--- bridge-connector/setup.py
+++ bridge-connector/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bridge-connector',
-    packages=['connector'],
+    packages=['connector', 'connector.client'],
     entry_points={
         'console_scripts': ['bridge-connector=connector.main:main'],
     }
