884527a8ab8cd7f1a6a279af27e5fbcc524d2889
enthyp <kuba.lanecki@gmail.com>
2020-11-11 20:10:09 +0100

    Require Python 3.8 and up.

--- bridge-connector/setup.py
+++ bridge-connector/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='bridge-connector',
     packages=['connector', 'connector.client'],
+    python_requires='>=3.8',
     entry_points={
         'console_scripts': ['bridge-connector=connector.main:main'],
     }
