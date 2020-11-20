f3fe5307f9da54c09ac811c5894120d70d1ccb3c
enthyp <kuba.lanecki@gmail.com>
2020-11-20 17:58:50 +0100

    Test server upgrade.
    Send callInfoUpdates and rosterUpdates with subscriptions indexes corresponding to their call IDs.
    Client should now be able to correctly label them and push to Kafka.

--- bridge-connector/run.sh
+++ bridge-connector/run.sh
@@ -7,4 +7,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 source .env
-bridge-connector --addresses localhost:8080 --logfile logs/logfile.json --kafka-file logs/kafka_file.json --dumpfile logs/dumpfile.json --no-ssl --kafka-bootstrap-server localhost:9092
+bridge-connector --addresses localhost:8080 --logfile logs/logfile.json --kafka-file logs/kafka_file.json --dumpfile logs/dumpfile.json --no-ssl
