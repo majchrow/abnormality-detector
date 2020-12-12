89dd16fbc6926e77194bc6c0c73d2a42acb040fc
enthyp <kuba.lanecki@gmail.com>
2020-12-12 15:15:16 +0100

    Added script for clean restart of Cassandra.

---
+++ scripts/erase_cassandra.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker-compose -f docker-compose-dev.yml rm -sfv cassandra
+docker volume rm abnormality-detector_cassandra_data
+docker-compose -f docker-compose-dev.yml up -d cassandra
+sleep 30
+docker-compose -f docker-compose-dev.yml up cassandra-load-keyspace
